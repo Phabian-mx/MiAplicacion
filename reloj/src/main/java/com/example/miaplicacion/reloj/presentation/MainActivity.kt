b8b9c8ed8b76575af87e2b1f3320f17125f16745
Phabian-mx <jose.lopez.25s@utzmg.edu.mx>
2026-06-25 21:50:41 -0600

    vibración y de sonido MP3

--- reloj/src/main/java/com/example/miaplicacion/reloj/presentation/MainActivity.kt
+++ reloj/src/main/java/com/example/miaplicacion/reloj/presentation/MainActivity.kt
@@ -5,21 +5,33 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.miaplicacion.reloj.R
-
-class MainActivity : ComponentActivity() { // Línea 1 de la diapositiva
-    override fun onCreate(savedInstanceState: Bundle?) { // Línea 2 de la diapositiva
-        super.onCreate(savedInstanceState) // Línea 5 de la diapositiva
-
-        // Enlaza el diseño XML clásico del reloj que ya editamos
-        setContentView(com.example.miaplicacion.reloj.R.layout.activity_main) // Línea 6 de la diapositiva
+import android.os.Vibrator
+import android.media.MediaPlayer
 
 
+class MainActivity : ComponentActivity() {
+    private lateinit var vibrator: Vibrator
+    private lateinit var mediaPlayer: MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(com.example.miaplicacion.reloj.R.layout.activity_main)
+
+        vibrator = getSystemService(android.content.Context.VIBRATOR_SERVICE) as Vibrator
+        mediaPlayer = MediaPlayer.create(this, R.raw.sonido)
         // 1. Buscamos el botón por el ID exacto de tu reloj ("btnAccionReloj")
         val boton: Button = findViewById(R.id.btnAccionReloj)
 
-        // 2. Programamos la acción del clic (Diapositiva 11)
+
         boton.setOnClickListener {
-            // Creamos la ruta para viajar desde esta ventana hacia "Prueba"
+            if (vibrator.hasVibrator()) {
+                vibrator.vibrate(android.os.VibrationEffect.createOneShot(500, android.os.VibrationEffect.DEFAULT_AMPLITUDE))
+            }
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+            }
+
             val intent = android.content.Intent(this, Prueba::class.java)
             startActivity(intent)
         }
